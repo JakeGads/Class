eed207abf3c0f962bf09f2ca4e64da41aaf914d8
Jake Gadaleta <jake.gads@gmail.com>
2018-08-24 08:58:09 -0400

    Initial commit

---
+++ Chapter 1/StringExample.java
@@ -0,0 +1,20 @@
+
+public class StringExample {
+	public static void main(String [] args) {
+		int 
+			myInt= 7;
+		double
+			myDouble = 7.8;
+		String 
+			text = "Hello",
+			blank = " ",
+			name = "Bob",
+			greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		System.out.println("Hello" + " " + "Bob");
+		System.out.println("My Integer is:\t" + myInt);
+		
+		System.out.println("my number is:\t" + myDouble + ".");
+	}
+}
---
+++ Chapter 1/UserInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class UserInput {
+	public static void main(String [] args) {
+		Scanner //creates the scanner object 
+			input = new Scanner(System.in);
+		
+		System.out.println("Enter a floating point value:\t"); //output promt
+		double
+			val = input.nextDouble(); //wait for user
+		
+		System.out.println("You entered " + val); //show the line
+		
+		input.close();
+	}
+}
